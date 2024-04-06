6bd1cd1e0f2be852a16f5c59cf5b7ca30b7c1891
mjkabir <mjkabir@gmail.com>
2024-04-06 06:48:01 +0000

    Working on better version

--- config.sh
+++ config.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Initialize DEBUG to 0
+DEBUG=0
+
+# Check if the --debug flag is passed
+
+for arg in "$@"; do
+  if [[ "$arg" == "--debug" ]]; then
+    DEBUG=1
+    shift
+  fi
+done
+
+function find_valid_rpc() {
+  for url in "$@"; do
+    response=$(curl -s "$url")
+    if [[ ! "$response" == *"exceeded limit for rpc"* ]]; then
+      export RPC="$url"
+      break
+    fi
+  done
+}
+
+
 debug() {
 
     if [ "$DEBUG" == "1" ]; then
@@ -8,26 +31,19 @@ debug() {
     fi
 }
 
-export DEBUG=1
+export DEBUG=$DEBUG
 export DIR=$HOME
 export HOST=`hostname`;
 
-# export RPC=https://api.mainnet-beta.solana.com
+RPC_NODES=( "https://mainnet.helius-rpc.com/?api-key=$RPC_API_KEY" "https://api.mainnet-beta.solana.com" )
 
-if [ -n "$RPC_API_KEY" ]; then
+find_valid_rpc "${RPC_NODES[@]}"
 
-    export RPC=https://mainnet.helius-rpc.com/?api-key=$RPC_API_KEY
-    debug "Using helius-rpc with your private RPC key"
-
-else
-
-    export RPC=https://api.mainnet-beta.solana.com
-    debug "Using default RPC"
-fi
-
+echo "DEBUG: $DEBUG RPC $url"
 
 export FEES=1000000
 export KEYPAIR=$HOME/my-solana-wallet/my-keypair.json
 export THREADS=4
 export ORE=`which ore`
 export SOLANA=`which solana`
+
--- mine.sh
+++ mine.sh
@@ -2,10 +2,31 @@
 
 source config.sh
 
+timeout_count=0
+
 while true; do
 
-    echo "Running ORE on $HOST for $HOME";
+    echo "$timeout_count running $ORE on $HOST";
 
-    $ORE --rpc $RPC --keypair $KEYPAIR --priority-fee $FEES mine --threads $THREADS </dev/null >/dev/null 2>&1
+
+    if [[ $DEBUG -eq 1 ]]; then
+
+       #$ORE --rpc $RPC --keypair $KEYPAIR --priority-fee $FEES mine --threads $THREADS | egrep -v '(Confirms|Attempt|Sending)'
+       timeout 60 $ORE --rpc $RPC --keypair $KEYPAIR --priority-fee $FEES mine --threads 8 | egrep -v '(Confirms|Attempt|Sending)'
+
+    else
+
+       $ORE --rpc $RPC --keypair $KEYPAIR --priority-fee $FEES mine --threads $THREADS </dev/null >/dev/null 2>&1
+
+    fi
+
+    # Check if the last command (timeout) returned 124, indicating a timeout occurred
+
+    if [ $? -eq 124 ]; then
+        ((timeout_count++))
+        echo "ORE timed out $timeout_count times."
+    fi
+
+
 
 done
